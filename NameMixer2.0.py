5abb72672449ccad9a9182b55798ad7db48308d0
Rinlix <46695556+Rinlix@users.noreply.github.com>
2019-01-14 19:21:22 -0500

    Add files via upload

---
+++ NameMixer2.0.py
@@ -0,0 +1,42 @@
+#NameMixer.py
+
+import random
+import time
+import sys
+confirmed = True
+while confirmed:
+    true = True
+    def rigged():
+        print('DToo much cringe and toxic, cannot survive...')
+        true = False
+    names = []
+    while true:
+        name = input('Enter A Name: [q] to randomize all names ')
+        if name == 'q':
+            num = len(names)
+            true = False
+        elif name == 'DonaldTrump':
+            rigged()
+        else:
+            names.append(name)
+
+    for a in range(num):
+        output = random.choice(names)
+        names.remove(output)
+        ordinal = a + 1
+        if ordinal == 1:
+            ordinal1 = '1st'
+        elif ordinal == 2:
+            ordinal1 = '2nd'
+        elif ordinal == 3:
+            ordinal1 = '3rd'
+        else:
+            ordinal = str(ordinal)
+            ordinal1 = (ordinal + 'th')
+        for i in range(6):
+            time.sleep(0.001)
+            print('=', end='')
+        print('', end='\n')
+        print('')
+        print(ordinal1,'is', ': ', output)
+        print('')
